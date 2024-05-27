e38376173e6e5f1dcf6da41735b943fb61ddb839
amal francis <amalfrancis744@gmail.com>
2024-05-27 22:02:30 +0530

    frontend added ok

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(cors({
-  origin: ["http://localhost:3001"],
+  origin: ["https://task-dirwatcher-nodejs.onrender.com/"],
   credentials:true
 }))
 
